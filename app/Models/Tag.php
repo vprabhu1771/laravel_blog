b1d139eb8058393fa5c6dd45aaa9fdb5fc2dd355
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-04 11:17:02 +0530

    Tag Model post relationship fixed

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -15,6 +15,6 @@ class Tag extends Model
 
     public function posts()
     {
-        return $this->morphedByMany(Post::class, 'taggable');
+        return $this->belongsToMany(Post::class, 'post_tag');
     }
 }
