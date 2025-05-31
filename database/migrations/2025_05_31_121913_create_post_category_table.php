c6be459875631caa55e8b9de7d8d147aa9631a14
Prabhu <vprabhu1771@gmail.com>
2025-05-31 18:02:58 +0530

    Create 2025_05_31_121913_create_post_category_table.php
    php artisan make:migration create_post_category_table

---
+++ database/migrations/2025_05_31_121913_create_post_category_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_category', function (Blueprint $table) {
+            $table->id();
+
+            // $table->foreignId('post_id')->constrained();
+            // $table->foreignId('category_id')->constrained();
+
+            $table->unsignedBigInteger('post_id');
+
+            $table->unsignedBigInteger('category_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_category');
+    }
+};
