a040d267ff3d19270f4cb1d60b784503a84c467c
Prabhu <vprabhu1771@gmail.com>
2025-06-03 12:26:39 +0530

    post detail dark and light mode added

--- resources/views/frontend/post/show.blade.php
+++ resources/views/frontend/post/show.blade.php
@@ -5,10 +5,16 @@
 @endsection
 
 @section('content')
-<h1>{{ $post->title }}</h1>
-@if($post->image_path)
-    <img src="{{ $post->GetImagePath() }}" class="img-fluid my-3" alt="{{ $post->title }}">
-@endif
-<p>{{ $post->content }}</p>
-<a href="{{ url()->previous() }}" class="btn btn-secondary mt-3">← Back</a>
+<div class="card bg-white dark:bg-gray-900 dark:text-white shadow rounded p-4">
+    
+    <h1>{{ $post->title }}</h1>
+
+    @if($post->image_path)
+        <img src="{{ $post->GetImagePath() }}" class="img-fluid my-3" alt="{{ $post->title }}">
+    @endif
+
+    <p>{{ $post->content }}</p>
+
+    <a href="{{ url()->previous() }}" class="btn btn-secondary mt-3">← Back</a>
+</div>
 @endsection
