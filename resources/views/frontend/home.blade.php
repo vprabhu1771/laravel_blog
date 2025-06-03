678a0eb1c32ae390f6c9f820d0f72248409018f4
Prabhu <vprabhu1771@gmail.com>
2025-06-03 12:26:26 +0530

    home page dark and light mode added

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -7,12 +7,16 @@ Home page
 @section('content')
 
 @foreach($posts as $row)
-<p>{{ $row->title }}</p>
-@if($row->image_path)
-    <img src="{{ $row->GetImagePath() }}" class="img-fluid my-3" alt="{{ $row->title }}">
-@endif
-<p>{{ Str::limit($row->content, 100) }}</p>
-<a href="{{ route('posts.show', $row->id) }}">Read More</a>
+
+<div class="card bg-white dark:bg-gray-900 dark:text-white shadow rounded p-4">
+    <h2 class="text-xl font-semibold">{{ $row->title }}</h2>
+    @if($row->image_path)
+        <img src="{{ $row->GetImagePath() }}" class="img-fluid my-3" alt="{{ $row->title }}">
+    @endif
+    <p class="text-sm">{{ Str::limit($row->description, 100) }}</p>
+    <a href="{{ route('posts.show', $row->id) }}" class="text-blue-600 dark:text-blue-400 underline mt-2 inline-block">Read More</a>
+</div>
+
 @endforeach
 
 @endsection
