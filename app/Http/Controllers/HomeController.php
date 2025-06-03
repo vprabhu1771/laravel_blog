71f7113d37025e8f2f78bdbbc7d3a2d4d9a7d319
Prabhu <vprabhu1771@gmail.com>
2025-06-03 12:16:48 +0530

    Display blog posts with a title, image, short description, and a "Read More" button

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Post;
+
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        return view('frontend/home');
+        $data = [
+            "posts" => Post::all()
+        ];
+
+        return view('frontend/home', $data);
     }
 }
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -5,5 +5,14 @@ Home page
 @endsection
 
 @section('content')
- home content
+
+@foreach($posts as $row)
+<p>{{ $row->title }}</p>
+@if($row->image_path)
+    <img src="{{ $row->GetImagePath() }}" class="img-fluid my-3" alt="{{ $row->title }}">
+@endif
+<p>{{ Str::limit($row->content, 100) }}</p>
+<a href="{{ route('posts.show', $row->id) }}">Read More</a>
+@endforeach
+
 @endsection
